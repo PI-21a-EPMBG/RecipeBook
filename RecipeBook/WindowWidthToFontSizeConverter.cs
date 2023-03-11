2a705b3f4e1d8bef29f2cc240f0494941586188d
Rerwkash <rewkash@gmail.com>
2023-03-11 08:28:51 +0300

    Сделан адаптивный интерфейс

---
+++ RecipeBook/WindowWidthToFontSizeConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace RecipeBook
+{
+    public class WindowWidthToFontSizeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double width = (double)value;
+            return width/50;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
