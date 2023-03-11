060a13e1889eaf80fd7e1252febe3725ce2487c1
Rerwkash <rewkash@gmail.com>
2023-03-11 20:36:21 +0300

    Полностью сделана адаптация

--- RecipeBook/WindowWidthToFontSizeConverter.cs
+++ RecipeBook/WindowWidthToFontSizeConverter.cs
@@ -5,17 +5,23 @@ using System.Windows.Data;
 
 namespace RecipeBook
 {
-    public class WindowWidthToFontSizeConverter : IValueConverter
+    public class WindowSizeToFontSizeConverter : IMultiValueConverter
     {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
         {
-            double width = (double)value;
-            return width/50;
+            double width = (double)values[0];
+            double height = (double)values[1];
+
+            // Выбираем минимальное значение между шириной и высотой окна
+            double size = Math.Min(width, height) / 50;
+
+            return size;
         }
 
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
         {
             throw new NotImplementedException();
         }
     }
+
 }
